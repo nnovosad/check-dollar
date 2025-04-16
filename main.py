9d97f7da1089d2aba05affa36eb63da9fb177d78
nnovosad <nnovosad19@gmail.com>
2025-04-16 15:40:16 +0300

    Update template; refactoring

--- main.py
+++ main.py
@@ -6,36 +6,26 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/index")
 async def get_index_page(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.post("/check/")
 async def check_file(request: Request, file: UploadFile = File(...)):
-    result = await get_result(file)  # Здесь обязательно await
+    result = await is_valid_banknote(file)  # Здесь обязательно await
 
     return templates.TemplateResponse(
         "result.html",
         {"result": result, "request": request}
     )
 
-
-async def get_result(file: UploadFile):
+async def is_valid_banknote(file: UploadFile):
     contents = await file.read()  # Читаем файл асинхронно
 
     # Преобразуем в numpy array и декодируем изображение
     nparr = np.frombuffer(contents, np.uint8)
     image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-    if image is None:
-        return "Не удалось загрузить изображение"
 
     # Обработка изображения
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -58,4 +48,4 @@ async def get_result(file: UploadFile):
         if (w <= 30 and h <= 30) or (area <= 900):
             valid_areas.append((x, y, w, h))
 
-    return "Купюра соответствует требованиям" if valid_areas else "Купюра не соответствует требованиям"
+    return True if valid_areas else False
